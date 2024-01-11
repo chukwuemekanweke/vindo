0815bd33f295db940154e26c8a036a3bd6719b19
Chukwuemeka Nweke <chukwuemeka.nweke@m-kopa.com>
2024-01-11 16:13:06 +0100

    Add project files.

---
+++ Vindo.Models/Domains/User/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Vindo.Models.Domains.User;
+
+public class UserDto
+{
+    public string Id { get; init; } = null!;
+    public string FirstName { get; init; } = null!;
+    public string LastName { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Username { get;init; } = null!;
+}
