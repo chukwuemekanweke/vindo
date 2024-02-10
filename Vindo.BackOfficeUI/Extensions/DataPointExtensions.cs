3d26a4b00ba1b941c6147e435a32f4cae7dbe524
Chukwuemeka Nweke <chukwuemeka.nweke@m-kopa.com>
2024-02-10 17:53:12 +0100

    Converted MorrisChart to component

---
+++ Vindo.BackOfficeUI/Extensions/DataPointExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Vindo.BackOfficeUI.Models.MorrisBarChart;
+
+namespace Vindo.BackOfficeUI.Extensions;
+
+public static class DataPointExtensions
+{
+	public static IEnumerable<Dictionary<string,object>> ToGraphDataPoints(this IEnumerable<DataPoint> dataPoints)
+	{
+		return dataPoints.Select(dataPoint =>
+		{
+			var obj = new Dictionary<string, object> { { "XKey", dataPoint.XKey } };
+			foreach (var kvp in dataPoint.Values)
+			{
+				obj[kvp.Key] = kvp.Value;
+			}
+			return obj;
+		});
+	}
+}
---
+++ Vindo.BackOfficeUI/Models/MorrisBarChart/DataPoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace Vindo.BackOfficeUI.Models.MorrisBarChart;
+
+public sealed class DataPoint
+{
+	public string XKey { get; set; } = null!;
+	public Dictionary<string, object> Values { get; set; }
+
+	public DataPoint()
+	{
+		Values = new Dictionary<string, object>();
+	}
+
+	public static List<DataPoint> ConvertRecordsToDataPoints(IEnumerable<object> records, string xAxisPropertyName)
+	{
+		return records.Select(record => FromRecord(record, xAxisPropertyName)).ToList();
+	}
+
+	public static DataPoint FromRecord(object record, string XKey)
+	{
+		if(record is null)
+		{
+			throw new ArgumentNullException(nameof(record));
+		}
+
+		DataPoint dataPoint = new DataPoint();
+		PropertyInfo? xAxisProperty = record.GetType()!.GetProperty(XKey);
+		if (xAxisProperty != null)
+		{
+			dataPoint.XKey = xAxisProperty.GetValue(record)?.ToString()?? throw new InvalidOperationException($"{xAxisProperty} cannot be null");
+		}
+		else
+		{
+			throw new ArgumentException($"Property '{XKey}' not found in record type.");
+		}
+
+		PropertyInfo[] properties = record.GetType().GetProperties();
+		foreach (PropertyInfo property in properties)
+		{
+			if (property.Name != XKey)
+			{
+				dataPoint.Values.Add(property.Name, Convert.ToInt32(property.GetValue(record)));
+			}
+		}
+
+		return dataPoint;
+	}
+}
