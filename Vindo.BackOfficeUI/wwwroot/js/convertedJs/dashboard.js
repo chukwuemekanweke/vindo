88c1f74cbfc93ac9303014d5965231655da7d64b
Chukwuemeka Nweke <chukwuemeka.nweke@m-kopa.com>
2024-02-10 14:47:12 +0100

    Implemented parent container progress bar circles

--- Vindo.BackOfficeUI/wwwroot/js/convertedJs/dashboard.js
+++ Vindo.BackOfficeUI/wwwroot/js/convertedJs/dashboard.js
@@ -82,78 +82,7 @@ function initializeDashboardCharts() {
 		axes: false,
 
 	});
-
-
-
-
-	//var bar = new ProgressBar.Circle(progressbar2, {
-	//	color: '#fb3d4e',
-	//	// This has to be the same size as the maximum width to
-	//	// prevent clipping
-	//	strokeWidth: 20,
-	//	trailWidth: 1,
-	//	easing: 'easeInOut',
-	//	duration: 1400,
-	//	text: {
-	//		autoStyleContainer: false
-	//	},
-	//	from: { color: '#5949d6', width: 1 },
-	//	to: { color: '#fb3d4e', width: 5 },
-	//	// Set default step function for all animate calls
-	//	step: function (state, circle) {
-	//		circle.path.setAttribute('stroke', state.color);
-	//		circle.path.setAttribute('stroke-width', state.width);
-
-	//		var value = Math.round(circle.value() * 110);
-	//		if (value === 0) {
-	//			circle.setText('');
-	//		} else {
-	//			circle.setText(value);
-	//		}
-
-	//	}
-	//});
-	//bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-	//bar.text.style.fontSize = '2rem';
-
-	//bar.animate(0.78);
-
-
-
-
-	//var bar = new ProgressBar.Circle(progressbar3, {
-	//	color: '#ea9715',
-	//	// This has to be the same size as the maximum width to
-	//	// prevent clipping
-	//	strokeWidth: 20,
-	//	trailWidth: 1,
-	//	easing: 'easeInOut',
-	//	duration: 1400,
-	//	text: {
-	//		autoStyleContainer: false
-	//	},
-	//	from: { color: '#5949d6', width: 1 },
-	//	to: { color: '#ea9715', width: 5 },
-	//	// Set default step function for all animate calls
-	//	step: function (state, circle) {
-	//		circle.path.setAttribute('stroke', state.color);
-	//		circle.path.setAttribute('stroke-width', state.width);
-
-	//		var value = Math.round(circle.value() * 90);
-	//		if (value === 0) {
-	//			circle.setText('');
-	//		} else {
-	//			circle.setText(value);
-	//		}
-
-	//	}
-	//});
-	//bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-	//bar.text.style.fontSize = '2rem';
-
-	//bar.animate(0.78);
-
-
+	
 
 
 
