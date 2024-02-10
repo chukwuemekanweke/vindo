ed7050d6ca8e61d5ae7c3790c1d5bf99ec1cb646
Chukwuemeka Nweke <chukwuemeka.nweke@m-kopa.com>
2024-02-10 14:11:25 +0100

    Fixed problem with progress circle bar

--- Vindo.BackOfficeUI/wwwroot/js/convertedJs/dashboard.js
+++ Vindo.BackOfficeUI/wwwroot/js/convertedJs/dashboard.js
@@ -4,82 +4,6 @@ function initializeDashboardCharts() {
 
 	'use strict';
 
-	var options = {
-		chart: {
-			height: 310,
-			type: 'line',
-			zoom: {
-				enabled: false
-			},
-			foreColor: "#8a99b5",
-		},
-		dataLabels: {
-			enabled: false
-		},
-		colors: ["#ff8f00", '#ee1044', '#389f99'],
-		stroke: {
-			width: [5, 7, 5],
-			curve: 'smooth',
-			dashArray: [0, 8, 5]
-		},
-		series: [{
-			name: "In Store",
-			data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
-		},
-		{
-			name: "Online",
-			data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
-		},
-		{
-			name: 'Total Visits',
-			data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
-		}
-		],
-		markers: {
-			size: 0,
-
-			hover: {
-				sizeOffset: 6
-			}
-		},
-		xaxis: {
-			categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-				'10 Jan', '11 Jan', '12 Jan'
-			],
-		},
-		tooltip: {
-			y: [{
-				title: {
-					formatter: function (val) {
-						return val + " (Avg)"
-					}
-				}
-			}, {
-				title: {
-					formatter: function (val) {
-						return val + " Avg"
-					}
-				}
-			}, {
-				title: {
-					formatter: function (val) {
-						return val;
-					}
-				}
-			}]
-		},
-		grid: {
-			borderColor: '#f1f1f1',
-		}
-	}
-
-	var chart = new ApexCharts(
-		document.querySelector("#sales-overview"),
-		options
-	);
-
-	//chart.render();
-
 
 	// Morris bar chart
 
@@ -162,72 +86,72 @@ function initializeDashboardCharts() {
 
 
 
-	var bar = new ProgressBar.Circle(progressbar2, {
-		color: '#fb3d4e',
-		// This has to be the same size as the maximum width to
-		// prevent clipping
-		strokeWidth: 20,
-		trailWidth: 1,
-		easing: 'easeInOut',
-		duration: 1400,
-		text: {
-			autoStyleContainer: false
-		},
-		from: { color: '#5949d6', width: 1 },
-		to: { color: '#fb3d4e', width: 5 },
-		// Set default step function for all animate calls
-		step: function (state, circle) {
-			circle.path.setAttribute('stroke', state.color);
-			circle.path.setAttribute('stroke-width', state.width);
+	//var bar = new ProgressBar.Circle(progressbar2, {
+	//	color: '#fb3d4e',
+	//	// This has to be the same size as the maximum width to
+	//	// prevent clipping
+	//	strokeWidth: 20,
+	//	trailWidth: 1,
+	//	easing: 'easeInOut',
+	//	duration: 1400,
+	//	text: {
+	//		autoStyleContainer: false
+	//	},
+	//	from: { color: '#5949d6', width: 1 },
+	//	to: { color: '#fb3d4e', width: 5 },
+	//	// Set default step function for all animate calls
+	//	step: function (state, circle) {
+	//		circle.path.setAttribute('stroke', state.color);
+	//		circle.path.setAttribute('stroke-width', state.width);
 
-			var value = Math.round(circle.value() * 110);
-			if (value === 0) {
-				circle.setText('');
-			} else {
-				circle.setText(value);
-			}
+	//		var value = Math.round(circle.value() * 110);
+	//		if (value === 0) {
+	//			circle.setText('');
+	//		} else {
+	//			circle.setText(value);
+	//		}
 
-		}
-	});
-	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-	bar.text.style.fontSize = '2rem';
+	//	}
+	//});
+	//bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+	//bar.text.style.fontSize = '2rem';
 
-	bar.animate(0.78);
-
-
+	//bar.animate(0.78);
 
 
-	var bar = new ProgressBar.Circle(progressbar3, {
-		color: '#ea9715',
-		// This has to be the same size as the maximum width to
-		// prevent clipping
-		strokeWidth: 20,
-		trailWidth: 1,
-		easing: 'easeInOut',
-		duration: 1400,
-		text: {
-			autoStyleContainer: false
-		},
-		from: { color: '#5949d6', width: 1 },
-		to: { color: '#ea9715', width: 5 },
-		// Set default step function for all animate calls
-		step: function (state, circle) {
-			circle.path.setAttribute('stroke', state.color);
-			circle.path.setAttribute('stroke-width', state.width);
 
-			var value = Math.round(circle.value() * 90);
-			if (value === 0) {
-				circle.setText('');
-			} else {
-				circle.setText(value);
-			}
 
-		}
-	});
-	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
-	bar.text.style.fontSize = '2rem';
+	//var bar = new ProgressBar.Circle(progressbar3, {
+	//	color: '#ea9715',
+	//	// This has to be the same size as the maximum width to
+	//	// prevent clipping
+	//	strokeWidth: 20,
+	//	trailWidth: 1,
+	//	easing: 'easeInOut',
+	//	duration: 1400,
+	//	text: {
+	//		autoStyleContainer: false
+	//	},
+	//	from: { color: '#5949d6', width: 1 },
+	//	to: { color: '#ea9715', width: 5 },
+	//	// Set default step function for all animate calls
+	//	step: function (state, circle) {
+	//		circle.path.setAttribute('stroke', state.color);
+	//		circle.path.setAttribute('stroke-width', state.width);
 
-	bar.animate(0.78);
+	//		var value = Math.round(circle.value() * 90);
+	//		if (value === 0) {
+	//			circle.setText('');
+	//		} else {
+	//			circle.setText(value);
+	//		}
+
+	//	}
+	//});
+	//bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+	//bar.text.style.fontSize = '2rem';
+
+	//bar.animate(0.78);
 
 
 
---
+++ Vindo.UI.Shared/DOMIdGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace Vindo.UI.Shared;
+
+public static class DOMIdGenerator
+{
+	public static string GenerateId()
+	{
+		Guid uniqueReference = Guid.NewGuid();
+		return $"{uniqueReference}".Replace("-","");
+	}
+}
